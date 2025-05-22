41a0b587f83f03eabf3ce0ffe6b20c2975eeeafa
hanifgaluh <hnfglh@gmail.com>
2025-05-22 19:59:22 +0700

    hapus port

--- src/components/PartnerSection.jsx
+++ src/components/PartnerSection.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import '../App.css';
-import dbsLogo from '../assets/partners/dbs.png';
-import dicodingLogo from '../assets/partners/dicoding.png';
-import gunadarmaLogo from '../assets/partners/gunadarma.png';
 
 function PartnerSection({ title = "Our Partner", partners }) {
   const defaultPartners = [
-    { id: 1, name: 'DBS Foundation', logo: dbsLogo },
-    { id: 2, name: 'Dicoding', logo: dicodingLogo },
-    { id: 3, name: 'Universitas Gunadarma', logo: gunadarmaLogo },
+    { id: 1, name: 'DBS Foundation'},
+    { id: 2, name: 'Dicoding'},
+    { id: 3, name: 'Universitas Gunadarma'},
   ];
 
   const partnerData = partners && partners.length > 0 ? partners : defaultPartners;
