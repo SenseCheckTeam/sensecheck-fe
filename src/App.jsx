aabeac12547296e96ca96b27925fc97cf3179d68
nfapriyanto <riyanchannel@gmail.com>
2025-05-23 17:57:25 +0700

    added dashboard admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,69 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-// Components
-import Layout from './components/Layout'
+// Import komponen
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import About from './pages/About';
 
-// Pages
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import About from './pages/About'
+// Import halaman sense
+import Penglihatan from './pages/Penglihatan';
+import Pendengaran from './pages/Pendengaran';
+import Penciuman from './pages/Penciuman';
+import Peraba from './pages/Peraba';
+import Pengecapan from './pages/Pengecapan';
 
-// Sense Pages
-import Penglihatan from './pages/Penglihatan'
-import Pendengaran from './pages/Pendengaran'
-import Penciuman from './pages/Penciuman'
-import Peraba from './pages/Peraba'
-import Pengecapan from './pages/Pengecapan'
+// Import halaman diagnosis
+import Diagnosis from './pages/Diagnosis';
+import DiagnosisForm from './pages/DiagnosisForm';
+import DiagnosisResult from './pages/DiagnosisResult';
 
-// Diagnosis Pages
-import Diagnosis from './pages/Diagnosis'
-import DiagnosisForm from './pages/DiagnosisForm'
-import DiagnosisResult from './pages/DiagnosisResult'
+// Import halaman admin
+import AdminLogin from './pages/AdminLogin';
+import AdminDashboard from './pages/AdminDashboard';
+import { AdminLoginGuard, AdminDashboardGuard } from './components/admin/AdminAuthGuard';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Register />} />
-
-          {/* Sense Routes */}
-          <Route path="penglihatan" element={<Penglihatan />} />
-          <Route path="pendengaran" element={<Pendengaran />} />
-          <Route path="penciuman" element={<Penciuman />} />
-          <Route path="peraba" element={<Peraba />} />
-          <Route path="pengecapan" element={<Pengecapan />} />
-
-          {/* Diagnosis Routes */}
-          <Route path="diagnosis" element={<Diagnosis />} />
-          <Route path="diagnosis/:senseType" element={<DiagnosisForm />} />
-          <Route path="diagnosis-result" element={<DiagnosisResult />} />
-
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
+      <div className="app-container">
+        <Routes>
+          {/* Admin Routes */}
+          <Route path="/admin/login" element={
+            <AdminLoginGuard>
+              <AdminLogin />
+            </AdminLoginGuard>
+          } />
+          <Route path="/admin/dashboard" element={
+            <AdminDashboardGuard>
+              <AdminDashboard />
+            </AdminDashboardGuard>
+          } />
+          <Route path="/admin" element={<Navigate to="/admin/login" replace />} />
+          
+          {/* Regular Routes with Layout */}
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="about" element={<About />} />
+            
+            {/* Sense Routes */}
+            <Route path="penglihatan" element={<Penglihatan />} />
+            <Route path="pendengaran" element={<Pendengaran />} />
+            <Route path="penciuman" element={<Penciuman />} />
+            <Route path="peraba" element={<Peraba />} />
+            <Route path="pengecapan" element={<Pengecapan />} />
+            
+            {/* Diagnosis Routes */}
+            <Route path="diagnosis" element={<Diagnosis />} />
+            <Route path="diagnosis/:senseType" element={<DiagnosisForm />} />
+            <Route path="diagnosis-result" element={<DiagnosisResult />} />
+          </Route>
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
